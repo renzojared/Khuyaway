7711162c475554d17894ff1573b943300fcef2b5
Renzo Jared <renzojared_lm@hotmail.com>
2024-10-14 00:44:34 -0500

    refactor: change output port to single method

--- src/package/Khuyaway.Boundaries/IOutputPort.cs
+++ src/package/Khuyaway.Boundaries/IOutputPort.cs
@@ -1,10 +1,6 @@
-using FluentValidation.Results;
-
 namespace Khuyaway.Boundaries;
 
 public interface IOutputPort<TResponse>
 {
     Task SuccessAsync(in TResponse? response, CancellationToken cancellationToken = default);
-    Task ValidationError(in IEnumerable<ValidationFailure> failures, CancellationToken cancellationToken = default);
-    Task ServerError(in Exception exception, CancellationToken cancellationToken = default);
 }
